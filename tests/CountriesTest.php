c5d7d3b1f454d98edcc73804104acb139bc38808
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-12-30 00:00:04 -0200

    Add currency test

--- tests/CountriesTest.php
+++ tests/CountriesTest.php
@@ -184,82 +184,11 @@ class CountriesTest extends TestCase
         );
     }
 
-    public function testMissingStates()
+    public function testCanGetCurrency()
     {
-        $this->assertNotEmpty(
-            Countries::where('cca3', 'BRA')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'ATG')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'ALA')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'ATG')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'BEL')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'BIH')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'ESH')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'IRQ')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'UNK')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'NOR')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'PNG')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'PRT')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'PSE')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'SJM')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'SOM')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'SRB')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'SSD')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'SYR')->first()->hydrate('states')->states
-        );
-
-        $this->assertEmpty(
-            Countries::where('cca3', 'UMI')->first()->hydrate('states')->states
+        $this->assertEquals(
+            'R$',
+            Countries::where('name.common', 'Brazil')->first()->currency->pluck('sign')[0]
         );
     }
 }
